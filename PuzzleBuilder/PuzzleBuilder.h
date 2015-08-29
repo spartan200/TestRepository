14d972c01f1331354c84dd44a3efd02de04e1c9b
Collin Thommasen <collinthommasen@Collins-MacBook-Air.local>
2015-08-29 14:50:26 -0700

    Initial Commit

---
+++ PuzzleBuilder/PuzzleBuilder.h
@@ -0,0 +1,19 @@
+//
+//  PuzzleBuilder.h
+//  PuzzleBuilder
+//
+//  Created by Collin Thommasen on 2015-08-29.
+//  Copyright (c) 2015 Collin Thommasen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PuzzleBuilder.
+FOUNDATION_EXPORT double PuzzleBuilderVersionNumber;
+
+//! Project version string for PuzzleBuilder.
+FOUNDATION_EXPORT const unsigned char PuzzleBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PuzzleBuilder/PublicHeader.h>
+
+
